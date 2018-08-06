541c4c176d0f6d8b5c649b7b319dacf546cea54e
gothicboy <870402548@qq.com>
2018-08-06 14:39:04 +0800

    '第二版'

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 
 from django.contrib.auth.hashers import make_password, check_password
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
-from app.models import UserModel, TicketModel, Goods
+from app.models import UserModel, TicketModel, Goods, CartModel
 from utils.functions import get_ticket
 
 def login(request):
@@ -72,7 +72,11 @@ def register(request):
 
 def cart(request):
     if request.method == 'GET':
-        return render(request,'cart.html')
+        user = request.user
+        carts = CartModel.objects.filter(user_id=user.id)
+
+        if user.id:
+            return render(request,'cart.html',{'carts':carts})
 
 def list(request):
     if request.method == 'GET':
@@ -94,4 +98,78 @@ def detail(request,gid):
     good = Goods.objects.get(gid=gid)
     return render(request,'detail.html',{'good':good,'gid':gid})
 
+def addtocart(request):
+    if request.method == 'POST':
+        user = request.user
+        data = {}
+        data['code'] = 1001
+        if user.id:
+            gid = request.POST.get('gid')
+            cart = CartModel.objects.filter(user=user,goods_id=gid).first()
+            if cart:
+                cart.c_num += 1
+                cart.save()
+                data['c_num'] = cart.c_num
+            else:
+                cart = CartModel.objects.create(user=user,goods_id=gid)
+                data['c_num'] = 1
+            data['code'] = 200
+            data['msg'] = '添加成功！'
+            return JsonResponse(data)
 
+        else:
+            data['msg'] = '请登录'
+        return JsonResponse(data)
+
+def goodsCount(request):
+    if request.method == 'GET':
+        user = request.user
+
+        carts = CartModel.objects.filter(user=user, is_select=True)
+        count_prices = 0
+        for cart in carts:
+            count_prices += cart.goods.gprice * cart.c_num
+
+        count_prices = round(count_prices, 3)
+        return JsonResponse({'count': count_prices, 'code': 200})
+
+def addcart(request):
+    if request.method == 'POST':
+        user = request.user
+        data = {}
+        data['code'] = 1001
+        gid = request.POST.get('gid')
+        cart = CartModel.objects.filter(user=user,goods_id=gid).first()
+        if cart:
+            cart.c_num += 1
+            cart.save()
+            data['c_num'] = cart.c_num
+            data['code'] = 200
+            data['c_num'] = cart.c_num
+            return JsonResponse(data)
+        return JsonResponse(data)
+
+
+def subcart(request):
+    if request.method == 'POST':
+        user = request.user
+        data = {}
+        data['code'] = 1001
+        gid = request.POST.get('gid')
+        cart = CartModel.objects.filter(user=user,goods_id=gid).first()
+        if cart:
+            cart.c_num -= 1
+            cart.save()
+            if cart.c_num == 0:
+                cart.delete()
+                data['c_num'] = 0
+                data['code'] = 200
+                return JsonResponse(data)
+            data['c_num'] = cart.c_num
+            data['code'] = 200
+            return JsonResponse(data)
+        return JsonResponse(data)
+
+def place_order(request):
+    if request.method == 'GET':
+        return render(request,'place_order.html')
